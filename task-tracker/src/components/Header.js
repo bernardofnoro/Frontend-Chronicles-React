7355ea026e5b5b22ef50edd79a0c61eb75e5adc2
bernardofnoro <bernardofn@outlook.com>
2021-12-07 18:49:22 -0300

    update1

---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <header>
+            <h1 style={headingstyle}>Task Tracker</h1>
+        </header>
+    )
+}
+
+const headingstyle = {
+    // insert css HERE
+}
+
+
+export default Header
